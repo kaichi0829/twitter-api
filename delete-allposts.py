bc1031a28f5fc2b06ab83eb38b6efe159fbc126d
kaichi0829 <kaichihasegawa.akishima@gmail.com>
2024-09-28 21:55:32 +0900

    edit delete-allposts.py

--- delete-allposts.py
+++ delete-allposts.py
@@ -5,30 +5,29 @@ api_key = 'DN4aq16BV1KjlbOkYz8Is8hEN'
 api_secret_key = 'uN3lZRdk6fBJBVSVSQ5H73qroQgOARHPGQKNgguLnV6ny2UqFG'
 access_token = '1816077751809630215-jqR6ikbR84com7FMlDOz6ThnhNgg2W'
 access_token_secret = 'pEWG2LSDOk9fuEp2F302MoHpL0Kl6RnCc3gMmSoKffi0c'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAABEKvAEAAAAA0g%2FWGtj8u%2B%2BTnIfPeHUR0O54ZLM%3DVqqdyoYilbieG2IhcPjOy4uA62cGJUhnxRQcL7dHdhPu7Fo0yR'  # v2ではbearer_tokenが必要
 
-# 2. 認証のセットアップ
-auth = tweepy.OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# 2. クライアントのセットアップ（v2）
+client = tweepy.Client(bearer_token=bearer_token, 
+                       consumer_key=api_key, 
+                       consumer_secret=api_secret_key,
+                       access_token=access_token, 
+                       access_token_secret=access_token_secret)
 
-# 3. ユーザーのタイムラインからツイートを取得
-username = 'X民意'  # 例: 'example_user'
-tweet_count = 10  # 取得するツイートの数
+# 3. ユーザーIDを取得（usernameから）
+username = 'X民意'  # 削除したいツイートのユーザー名
+user = client.get_user(username=username)
+user_id = user.data.id
 
-try:
-    tweets = api.user_timeline(screen_name=username, count=tweet_count)
+# 4. ユーザーのツイートを取得
+tweet_count = 10  # 取得するツイート数
+tweets = client.get_users_tweets(id=user_id, max_results=tweet_count)
 
-    # 取得したツイートを表示
-    for tweet in tweets:
-        print(f"Tweet ID: {tweet.id} - Tweet Text: {tweet.text}")
+# 5. 取得したツイートの表示
+for tweet in tweets.data:
+    print(f"Tweet ID: {tweet.id} - Tweet Text: {tweet.text}")
 
-    # 4. 削除するツイートのIDを指定
-    # ここでは、削除したいツイートを指定
-    tweet_id_to_delete = tweets[0].id  # 例として最新のツイートを削除
-
-    # 5. ツイートを削除
-    api.destroy_status(tweet_id_to_delete)
-    print(f'Tweet with ID {tweet_id_to_delete} has been deleted.')
-
-except Exception as e:
-    print(f'Error: {e}')
+# 6. 削除するツイートのIDを指定して削除
+tweet_id_to_delete = tweets.data[0].id  # 例として最新ツイートを削除
+client.delete_tweet(id=tweet_id_to_delete)
+print(f'Tweet with ID {tweet_id_to_delete} has been deleted.')
