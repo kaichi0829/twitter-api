7ecf115573d7e41a85409e7912e82d8491b7b5f1
kaichi0829 <kaichihasegawa.akishima@gmail.com>
2024-09-28 21:34:50 +0900

    edit delete-allposts.py

--- delete-allposts.py
+++ delete-allposts.py
@@ -1,33 +1,23 @@
 import tweepy
-import json
-import requests
-from requests_oauthlib import OAuth1
 
-# APIキーとトークンの設定
-API_KEY = 'DN4aq16BV1KjlbOkYz8Is8hEN'
-API_SECRET_KEY = 'uN3lZRdk6fBJBVSVSQ5H73qroQgOARHPGQKNgguLnV6ny2UqFG'
-ACCESS_TOKEN = '1816077751809630215-jqR6ikbR84com7FMlDOz6ThnhNgg2W'
-ACCESS_TOKEN_SECRET = 'pEWG2LSDOk9fuEp2F302MoHpL0Kl6RnCc3gMmSoKffi0c'
+# Twitter API認証情報
+consumer_key = 'DN4aq16BV1KjlbOkYz8Is8hEN'
+consumer_secret = 'uN3lZRdk6fBJBVSVSQ5H73qroQgOARHPGQKNgguLnV6ny2UqFG'
+access_token = '1816077751809630215-jqR6ikbR84com7FMlDOz6ThnhNgg2W'
+access_token_secret = 'pEWG2LSDOk9fuEp2F302MoHpL0Kl6RnCc3gMmSoKffi0c'
 
-# Twitter API認証
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# Twitter APIの認証
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-# API接続
+# APIインスタンスの作成
 api = tweepy.API(auth)
 
-def delete_all_tweets():
-    # ツイート取得と削除
-    for status in tweepy.Cursor(api.user_timeline).items():
-        try:
-            api.destroy_status(status.id)
-            print(f"Deleted tweet: {status.id}")
-        except tweepy.TweepError as e:
-            print(f"Failed to delete tweet {status.id}: {e}")
-        
-        # API制限を回避するための待機
-        time.sleep(1)
+# 最新の5件のツイートを取得して削除
+tweets = api.user_timeline(count=5)
+for tweet in tweets:
+    print(f"Deleting tweet: {tweet.text}")
+    api.destroy_status(tweet.id)
+    print("Tweet deleted successfully")
 
-if __name__ == "__main__":
-    delete_all_tweets()
-    print("All tweets have been deleted.")
+print("Deletion of the 5 most recent tweets completed.")
